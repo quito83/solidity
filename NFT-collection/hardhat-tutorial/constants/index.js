fe99452e917cf3618bec6037d8417a1b78b22acb
Quito <quitoapps@gmail.com>
2022-10-18 16:28:58 +0000

    Pre Deploy a Goerli

---
+++ NFT-collection/hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xf2C0eFaAD4f4B728B3F0525D9848dE2745a661aA";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
