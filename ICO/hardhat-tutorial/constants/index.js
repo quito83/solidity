5f54ee4a423320671cd927da897128a8cd7ef303
Quito <quitoapps@gmail.com>
2022-10-25 16:04:38 +0000

    Next app

---
+++ ICO/hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x8a4B711C8552E3658F83e9941188eB8B616d9999";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
